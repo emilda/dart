e212789308c8200499b7b3364db81aff09e46603
Sindhu Emilda <emildasindhu@gmail.com>
2016-04-30 23:03:43 -0400

    new files and scotts changes

--- src/main/resources/static/scripts.js
+++ src/main/resources/static/scripts.js
@@ -27,13 +27,6 @@ function GoogleMapInit(id, latitude, longitude, address)
       if (status == google.maps.GeocoderStatus.OK) 
       {
          map.setCenter(results[0].geometry.location);
-         var marker = new google.maps.Marker(
-         {
-            map: map, 
-            position: results[0].geometry.location
-         });
-         //populate Travelers field offices
-         findFieldOffice();
       } 
       else 
       {
@@ -55,22 +48,18 @@ function GoogleMapRecall(address) {
         if (status == google.maps.GeocoderStatus.OK) 
         {
             map.setCenter(results[0].geometry.location); 
-            map.setZoom(16);
-            var marker = new google.maps.Marker({
-                map: map, 
-                position: results[0].geometry.location
-            });
+            map.setZoom(14);
             var cityCircle = new google.maps.Circle({
-                strokeColor: '#FF0000',
+                strokeColor: '#496775',
                 strokeOpacity: 0.8,
                 strokeWeight: 2,
-                fillColor: '#FF0000',
-                fillOpacity: 0.35,
+                fillColor: '#496775',
+                fillOpacity: 0.15,
                 map: map,
-                //center: {lat: 35.4676, lng: -97.5164},
                 center: results[0].geometry.location,
-                radius: Math.sqrt(10000) * 100
+                radius: Math.sqrt(10000) * 15
               });
+            callFindClaimSvcByzip();
         } 
         else 
         {
@@ -79,7 +68,7 @@ function GoogleMapRecall(address) {
     });
 }
 
-function callClaimSvcByzip()
+function callFindClaimSvcByzip()
 {
         //alert("In claim");
 		var claimSvcURL = "/custbypostalcode/"+ addy;
@@ -103,6 +92,31 @@ function callClaimSvcByzip()
         return (false);
 
     }
+
+function callClaimSvcByzip()
+{
+        //alert("In claim");
+		var claimSvcURL = "/custbypostalcode/"+ addy;
+        var returnData = "";
+        $.ajax({
+            type: "GET",
+            dataType: 'jsonp',
+			jsonp: 'callback',
+			jsonpCallback: 'jsonpCallbackWithSeverity',
+            async: true,
+            url: claimSvcURL,
+            success: function(data) {
+					//alert("Server said123:\n '" + data + "'");
+            },
+			error: function(e){  
+					alert('Error121212: ' + e);
+					console.log(e);
+					
+       }  
+        });
+        return (false);
+
+    }
 function jsonpCallback(data) {
 	//alert("in callback");
 
@@ -110,9 +124,22 @@ function jsonpCallback(data) {
 	insuredlist = data;
 	console.log(insuredlist);
 	console.log(insuredlist.Customer_zip.length);
-	buildInsured(insuredlist);
+	buildInsured(insuredlist, 'no');
 }
-function buildInsured(insuredlist) {
+
+function jsonpCallbackWithSeverity(data) {
+	//alert("in callback");
+
+	console.log(data);
+	insuredlist = data;
+	console.log(insuredlist);
+	console.log(insuredlist.Customer_zip.length);
+	buildInsured(insuredlist, 'yes');
+	map.controls[google.maps.ControlPosition.RIGHT_TOP].push(document.getElementById('legend'));
+	getBestPoint(35.474223, -97.521404, 35.469994, -97.510719);
+}
+
+function buildInsured(insuredlist, hasSeverity) {
    console.log("in build insured");
    var insured = 0;
    var _lat;
@@ -121,14 +148,34 @@ function buildInsured(insuredlist) {
 	  var parsed_lat_long = { lat:parseFloat( insuredlist.Customer_zip[insured].position.substring(0, 9) ), lng:parseFloat( insuredlist.Customer_zip[insured].position.substring(10, 19)  ) };
 	  var insuredCust = insuredlist.Customer_zip[insured];
 	  console.log(parsed_lat_long);
-	  createMarker(insuredCust, parsed_lat_long);
+	  createMarker(insuredCust, parsed_lat_long, hasSeverity);
 	 };
+	 
+	 if (hasSeverity =='yes'){
+	//CREATE legend
+		var legend = document.getElementById('legend');
+		var div = document.createElement('div');
+		div.innerHTML = '<img src="redHome.png"> ' + '<b>SEVERE</b>';
+		legend.appendChild(div);
+		div = document.createElement('div');
+		div.innerHTML = '<img src="yellowHome.png"> ' + '<b>MODERATE</b>';
+		legend.appendChild(div);
+		div = document.createElement('div');
+		div.innerHTML = '<img src="greenHome.png"> ' + '<b>MILD</b>';
+		legend.appendChild(div);
+		div = document.createElement('div');
+		div.innerHTML = '<img src="blueHome.png"> ' + '<b>NO DATA</b>';
+		legend.appendChild(div);
+	 }
 }
  		  
   //call createMarker
-function createMarker(insuredCust, position) {
+function createMarker(insuredCust, position, hasSeverity) {
   	var infowindow = new google.maps.InfoWindow();
-  	var iconcolor = determineDamageColor(insuredCust.severity);
+ 	var iconcolor = "blueHome.png";
+ 	if (hasSeverity == 'yes'){
+ 		iconcolor = determineDamageColor(insuredCust.severity);
+ 	}
   	var marker = new google.maps.Marker({
 		icon: iconcolor,
     	position: position,
@@ -138,44 +185,59 @@ function createMarker(insuredCust, position) {
      		
   	marker.addListener('click', function() {
 		//creates infowindow
-  		createInfoWindow(insuredCust, marker);
-		//map.setCenter(marker.getPosition());
+  		createInfoWindow(insuredCust, marker, hasSeverity);
 	   	});
    }
  		   		 
 
-function createInfoWindow(insuredCust, marker) {
+function createInfoWindow(insuredCust, marker, hasSeverity) {
+	var txt = '<IMG BORDER="0" ALIGN="Left" SRC="icon.png">' + 
+			  "Insured: " +  insuredCust.name + "<br />" +  
+			  "Policy #: " + insuredCust.policyNbr + "<br />" +
+			  "Phone: " + insuredCust.telephoneNbr;
+	var txtWithSeverity =  txt +  '<IMG BORDER="0" ALIGN="Left" SRC="' + insuredCust.url_img + '">';	  
+	var contentString = txt;
+	if (hasSeverity == 'yes'){
+		contentString = txtWithSeverity;
+	}
 	var infowindow = new google.maps.InfoWindow({
-   		content:'<IMG BORDER="0" ALIGN="Left" SRC="icon.png">' + 
-  				"Insured: " +  insuredCust.name + "<br />" + 
-  				"Policy #: " + insuredCust.policyNbr + "<br />" +
-  				"Contact Info: " + insuredCust.telephoneNbr + "<br />" +  
-  				'<IMG BORDER="0" ALIGN="Left" SRC="' + insuredCust.url_img + '">'
-  	}); 
-  	infowindow.open(map, marker);	
-   }      
+	    content: contentString,
+	  });
+	
+	infowindow.open(map, marker);	
+}
     
 //determine icon color based on severity
 function determineDamageColor(severity) {
-  	 var iconcolor = "blueHome.png"; 	  
   	 if(severity > 7){
-  	 	iconcolor = 'greenHome.png';
-  	 }
-  	 else if(severity <= 7 && severity > 4){
-  	  	iconcolor = 'yellowHome.png';
-  	 }
-   	 else if(severity <= 4){
-   	  	iconcolor = 'redHome.png';
-  	 }
+	  	 	iconcolor = 'redHome.png';
+	  	 }
+	  	 else if(severity <= 7 && severity > 4){
+	  	  	iconcolor = 'yellowHome.png';
+	  	 }
+	   	 else if(severity <= 4 && severity >0){
+	   	  	iconcolor = 'greenHome.png';
+	  	 }
+	   	 else{
+	   		 iconcolor= 'blueHome.png';
+	   	 }
 	return iconcolor;
 }
 
-function findFieldOffice(){
+function getBestPoint(lat1, lon1, lat2, lon2) {
+	var newlat = (lat1 + lat2)/2;
+	var newlon = (lon1 + lon2)/2;
+	var locate = new google.maps.LatLng(parseFloat(newlat), parseFloat(newlon));
 	var marker = new google.maps.Marker({
-		icon: "icon.png",
-    	position: {lat: 38.889956, lng: -94.673202},
+		icon: "bestpointicon.png",
+		animation: google.maps.Animation.DROP,
+		//size: new google.maps.Size(20, 32),
+    	position: locate,
+      	title: "Best Point",
       	map: map
    });
+	map.setZoom(15);
+	
 }
 
 function callDamageAlgorithm(){
